b876c48df5741742afa1c43acbfc0e9b1e9cb471
EzeTorcetta <ezequiel.torcetta.97@gmail.com>
2023-06-08 11:01:45 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001 (force: false)'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`Server raised in port ${PORT}`);; // eslint-disable-line no-console
   });
 });
